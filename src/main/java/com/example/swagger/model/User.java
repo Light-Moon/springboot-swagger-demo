694b513ab018e8600e9c3f15885003a10b03d69d
Light-Moon <1424686850@qq.com>
2018-12-01 13:01:46 +0800

    Demo including basic functions.

---
+++ src/main/java/com/example/swagger/model/User.java
@@ -0,0 +1,48 @@
+package com.example.swagger.model;
+
+import java.util.Date;
+
+public class User {
+    private int id;
+    private String name;
+    private int age;
+    private Date time;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", name=" + name + ", age=" + age + ", time=" + time + "]";
+    }
+
+}
